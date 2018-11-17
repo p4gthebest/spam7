0dc27440897951437d3dfda1874d4dfd70660608
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-17 14:15:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,102 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("P4G the best");
 
 
-client.on('message', message => {
-    if(message.content === 'حيدر'){
-        message.channel.send('يوسف')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'يوسف'){
-        message.channel.send('الو')
-    }
-});
-
-
+client.on("ready", () => {
+let channel =     client.channels.get("513309819612954624")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
 
 client.on('message', message => {
-    if(message.content === 'الو'){
-        message.channel.send('هلا')
-    }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send('كيفك')
-    }
-});
-
-
-
-
-client.on('message', message => {
-    if(message.content === 'كيفك'){
-        message.channel.send('تمام')
-    }
-});
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === 'حيدر'){
+    if(message.content === 'check'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === 'يوسف'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'الو'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'كيفك'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'تمام'){
-        message.channel.send('#daily')
-    }
-});
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === '#daily'){
+    if(message.content === 'check'){
         message.channel.send('#credit')
     }
 });
 
-
-client.on('message', message => {
-    if(message.content === '#credit'){
-        message.channel.send('حيدر')
-    }
-});
 
 
 
