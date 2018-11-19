65b92ec9c53b31fe2e8401956fd44812093290b4
p4gthebest <41386213+p4gthebest@users.noreply.github.com>
2018-11-19 22:20:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("P4G is the best");
+console.log("P4G the best");
 
 
 client.on("ready", () => {
@@ -9,6 +9,41 @@ setInterval(function() {
 channel.send(`**Road To 100k Credit**`);
 }, 25)
 })
+
+client.on("ready", () => {
+let channel =     client.channels.get("514185149240311821")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
+client.on("ready", () => {
+let channel =     client.channels.get("514185186041135123")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
+client.on("ready", () => {
+let channel =     client.channels.get("514185204231831592")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
+client.on("ready", () => {
+let channel =     client.channels.get("514185218748579841")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
+client.on("ready", () => {
+let channel =     client.channels.get("514185264160178186")
+setInterval(function() {
+channel.send(`**Road To 100k Credit**`);
+}, 25)
+})
+
+
+
+
 
 client.on('message', message => {
     if(message.content === 'check'){
